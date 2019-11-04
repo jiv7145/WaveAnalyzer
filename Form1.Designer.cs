f3ee5ec7f0f5145726d6bd60a596df933aba3421
Jake Lim <jiv7145@gmail.com>
2019-11-04 14:32:41 -0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WaveAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.hScrollBar1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1255, 442);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Time Domain";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Location = new System.Drawing.Point(8, 23);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1239, 383);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 410);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(1255, 23);
+            this.hScrollBar1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel2);
+            this.groupBox2.Controls.Add(this.hScrollBar2);
+            this.groupBox2.Location = new System.Drawing.Point(24, 465);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(1247, 471);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Frequency Domain";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(8, 23);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1231, 416);
+            this.panel2.TabIndex = 1;
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(0, 443);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(1247, 23);
+            this.hScrollBar2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(1279, 15);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(303, 919);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Controls";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(8, 3);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(181, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1597, 951);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Wave Analyzer";
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WaveAnalyzer {
+    public partial class Form1: Form {
+        Graphics gPanel1;
+        float[] output;
+
+        public Form1() {
+            InitializeComponent();
+            gPanel1 = panel1.CreateGraphics();
+            ReadWavFile("./sample.wav", out float[] L, out float[] R);
+            output = L;
+        }
+
+
+        private void groupBox1_Enter(object sender, EventArgs e) {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e) {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void chart2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) {
+            
+            Pen pen = new Pen(Brushes.Blue, 0.1F);
+            float x1 = 0;
+            float y1 = 70;
+
+            float y2 = 0;
+
+            //for (float x2 = 0; x2 < panel1.Width; x2 += 1F) {
+            //    //y2 = (float)Complex.Pow(Math.Sin(x), x).Real;
+            //    int i = 0;
+            //    y2 = output[i] * 1000 + 100;
+
+            //    gPanel1.DrawLine(pen, x1, y1, x2, y2);
+            //    x1 = x2;
+            //    y1 = y2;
+            //    i++;
+            //}
+            for (int x2 = 0; x2 < panel1.Width; x2+=1) {
+                y2 = output[x2] * 5000 + 150;
+
+                gPanel1.DrawLine(pen, x1, y1, x2, y2);
+                x1 = x2;
+                y1 = y2;
+            }
+
+
+        }
+
+
+
+
+
+        public static bool ReadWavFile(string filename, out float[] L, out float[] R) {
+            L = R = null;
+
+            try {
+                using (FileStream fs = File.Open(filename, FileMode.Open)) {
+                    BinaryReader reader = new BinaryReader(fs);
+
+                    // chunk 0
+                    int chunkID = reader.ReadInt32();
+                    int fileSize = reader.ReadInt32();
+                    int riffType = reader.ReadInt32();
+
+
+                    // chunk 1
+                    int fmtID = reader.ReadInt32();
+                    int fmtSize = reader.ReadInt32(); // bytes for this chunk
+                    int fmtCode = reader.ReadInt16();
+                    int channels = reader.ReadInt16();
+                    int sampleRate = reader.ReadInt32();
+                    int byteRate = reader.ReadInt32();
+                    int fmtBlockAlign = reader.ReadInt16();
+                    int bitDepth = reader.ReadInt16();
+
+                    if (fmtSize == 18) {
+                        // Read any extra values
+                        int fmtExtraSize = reader.ReadInt16();
+                        reader.ReadBytes(fmtExtraSize);
+                    }
+
+
+                    // chunk 2 -- HERE'S THE NEW STUFF (ignore these subchunks, I don't know what they are!)
+                    int bytes;
+                    while (new string(reader.ReadChars(4)) != "data") {
+                        bytes = reader.ReadInt32();
+                        reader.ReadBytes(bytes);
+                    }
+
+                    // DATA!
+                    bytes = reader.ReadInt32();
+                    byte[] byteArray = reader.ReadBytes(bytes);
+
+                    int bytesForSamp = bitDepth / 8;
+                    int samps = bytes / bytesForSamp;
+
+
+                    float[] asFloat = null;
+                    switch (bitDepth) {
+                        case 64:
+                            double[]
+                            asDouble = new double[samps];
+                            Buffer.BlockCopy(byteArray, 0, asDouble, 0, bytes);
+                            asFloat = Array.ConvertAll(asDouble, e => (float)e);
+                            break;
+                        case 32:
+                            asFloat = new float[samps];
+                            Buffer.BlockCopy(byteArray, 0, asFloat, 0, bytes);
+                            break;
+                        case 16:
+                            Int16[]
+                            asInt16 = new Int16[samps];
+                            Buffer.BlockCopy(byteArray, 0, asInt16, 0, bytes);
+                            asFloat = Array.ConvertAll(asInt16, e => e / (float)Int16.MaxValue);
+                            break;
+                        default:
+                            return false;
+                    }
+
+                    switch (channels) {
+                        case 1:
+                            L = asFloat;
+                            R = null;
+                            return true;
+                        case 2:
+                            L = new float[samps];
+                            R = new float[samps];
+                            for (int i = 0, s = 0; i < samps; i++) {
+                                L[i] = asFloat[s++];
+                                R[i] = asFloat[s++];
+                            }
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+            }
+            catch {
+                Debug.WriteLine("...Failed to load note: " + filename);
+                return false;
+                //left = new float[ 1 ]{ 0f };
+            }
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e) {
+            //int val = trackBar1.Value;
+        }
+    }
+}
